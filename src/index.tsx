89854219da6c6e29467939d37ccf3b243722c4fb
aouahib <ouahibdou@gmail.com>
2021-02-16 22:30:06 +0100

    Put index back to /src (oops)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
-import reportWebVitals from '../reportWebVitals';
+import App from './app/app';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 ReactDOM.render(
